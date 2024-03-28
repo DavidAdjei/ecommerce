1bfaa3cb7ae823ae7efda54aad2262f08e03a516
DavidAdjei <97539347+DavidAdjei@users.noreply.github.com>
2024-03-28 23:48:12 +0000

    huh

--- Server/index.js
+++ Server/index.js
@@ -17,8 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: [""],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
 }));
 app.use(morgan("dev"));
+app.options('*', cors());
 
 app.get('/', (req, res) => {
     res.send('Server is running');

9921ea3d2f6ed4d11bd483bea2f14c3e9ca77ffc
SOLOMON CUDJOE <scudjoe016@st.ug.edu.gh>
2024-09-27 17:17:29 +0000

    Made a little changes for now

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,50 +1,58 @@
-import PropTypes from 'prop-types'
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
-import { Route, Routes } from 'react-router-dom';
-import './App.css'; 
-import Homepage from './Pages/HomePage/Homepage';
-import NavBar from './component/NavBar';
-import Footer from './component/footer';
-import { getProducts, setFeaturedProducts } from './redux/Actions/productActions';
-import ProductsPage from './Pages/ProductsPage/ProductsPage';
-import ProductDetails from './Pages/ProductDetails/ProductDetails';
-import Cart from './Pages/Cart/Cart';
+import PropTypes from "prop-types";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Homepage from "./Pages/HomePage/Homepage";
+import NavBar from "./component/NavBar";
+import Footer from "./component/footer";
+import {
+  getProducts,
+  setFeaturedProducts,
+} from "./redux/Actions/productActions";
+import ProductsPage from "./Pages/ProductsPage/ProductsPage";
+import ProductDetails from "./Pages/ProductDetails/ProductDetails";
+import Cart from "./Pages/Cart/Cart";
+import Contact from "./Pages/Contact/Contact";
+import About from "./Pages/About/About";
 
 const App = ({ getProducts, setFeaturedProducts }) => {
   useEffect(() => {
-    getProducts().then((res) => {
-      const popular = res.products.filter(product => product.popular === true);
-      console.log(popular);
-      setFeaturedProducts(popular);
-    }).catch((err => console.log(err)));
-  },[getProducts, setFeaturedProducts])
+    getProducts()
+      .then((res) => {
+        const popular = res.products.filter(
+          (product) => product.popular === true
+        );
+        console.log(popular);
+        setFeaturedProducts(popular);
+      })
+      .catch((err) => console.log(err));
+  }, [getProducts, setFeaturedProducts]);
   return (
-    <div className='App'>
-        <NavBar />
-        <div className='main'>
-          <Routes>
-            <Route path="/" exact element={<Homepage/>} />
-            <Route path="/product/:id" element={<ProductDetails/>} />
-            <Route path="/all-products" element={<ProductsPage/>} /> 
-            <Route path='/cart' element={ <Cart/> } />
-          </Routes> 
+    <div className="App">
+      <NavBar />
+      <div className="main">
+        <Routes>
+          <Route path="/" exact element={<Homepage />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/shop" element={<ProductsPage />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
-      <Footer/>
-        
+      <Footer />
     </div>
-    
-  )
-}
+  );
+};
 
 App.propTypes = {
   getProducts: PropTypes.func,
-  setFeaturedProducts: PropTypes.func
-}
+  setFeaturedProducts: PropTypes.func,
+};
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state) => ({});
 
-})
-
-
-export default connect(mapStateToProps, {getProducts, setFeaturedProducts})(App)
+export default connect(mapStateToProps, { getProducts, setFeaturedProducts })(
+  App
+);
--- Client/src/component/NavBar.jsx
+++ Client/src/component/NavBar.jsx
@@ -1,27 +1,52 @@
-import React from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import "./component.css";
-import Logo from '../assets/images/Logo.jpeg';
-
+import Logo from "../assets/images/Logo.jpeg";
+import { IoPersonOutline } from "react-icons/io5";
+import { IoCartOutline } from "react-icons/io5";
+import { IoIosSearch } from "react-icons/io";
 
 export default function NavBar() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const handleSignUpClick = () => {
-    console.log("Clicked")
+    console.log("Clicked");
   };
 
   return (
-    <header className='header'>
-      <nav className='nav'>
-        <h1 className='nav_welcome'><Link to="/" className='nav_logo'><img src={Logo} alt="Logo" /></Link></h1>
-          <div className='button_container'>
-            <button onClick={ () => navigate('/cart') }>Cart</button>
-            <button onClick={handleSignUpClick}>Sign Up</button>
-            <button >Log In</button>
-          </div>
+    <header className="header">
+      <nav className="nav">
+        <div className="left">
+          <h1 className="nav_welcome">
+            <Link to="/" className="nav_logo">
+              <img src={Logo} alt="Logo" />
+            </Link>
+          </h1>
+          <Link to="/">Home</Link>
+          <Link to="/shop">Shop</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+
+        <div className="right">
+          <input
+            className="search_input"
+            type="text"
+            placeholder="search..."
+            autoCorrect="false"
+            autoComplete="false"
+          />
+          <Link>
+            <IoIosSearch size={30} className="search_button" />
+          </Link>
+          <Link to="/profile">
+            <IoPersonOutline size={30} />
+          </Link>
+          <Link to="/cart">
+            <IoCartOutline size={30} />
+          </Link>
+        </div>
       </nav>
-      
     </header>
   );
 }

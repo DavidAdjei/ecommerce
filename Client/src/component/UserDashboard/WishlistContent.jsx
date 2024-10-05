412f27279c8c5cdd737f0453376c1646c24859da
SOLOMON CUDJOE <scudjoe016@st.ug.edu.gh>
2024-10-05 17:05:45 +0000

    Correct error with wishlist

--- Client/src/component/UserDashboard/WishlistContent.jsx
+++ Client/src/component/UserDashboard/WishlistContent.jsx
@@ -1,26 +1,44 @@
-import PropTypes from 'prop-types'
- import React, { useEffect, useState } from 'react';
-import { Box, Card, CardMedia, CardContent, Typography, Button, Grid2 } from '@mui/material';
-import { Delete, ShoppingCart } from '@mui/icons-material';
-import { connect } from 'react-redux';
-import { getWishlist, removeFromWishlist } from '../../redux/Actions/authActions';
-import { addToCart } from '../../redux/Actions/cartActions';
-import { setFeedback } from '../../redux/Actions/productActions';
-import Loader from '../../features/Loader';
+import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Button,
+  Grid2,
+} from "@mui/material";
+import { Delete, ShoppingCart } from "@mui/icons-material";
+import { connect } from "react-redux";
+import {
+  getWishlist,
+  removeFromWishlist,
+} from "../../redux/Actions/authActions";
+import { addToCart } from "../../redux/Actions/cartActions";
+import { setFeedback } from "../../redux/Actions/productActions";
+import Loader from "../../features/Loader";
 
-const WishlistContent = ({user, wishList, getWishlist, removeFromWishlist, addToCart, setFeedback}) => {
+const WishlistContent = ({
+  user,
+  wishList,
+  getWishlist,
+  removeFromWishlist,
+  addToCart,
+  setFeedback,
+}) => {
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     setLoading(true);
     getWishlist(user._id)
       .catch((error) => setFeedback(error))
-      .finally(() => setLoading(false)); 
+      .finally(() => setLoading(false));
   }, [getWishlist, user, setFeedback]);
 
   const handleRemove = (productId) => {
     removeFromWishlist(user._id, productId)
       .then((res) => setFeedback(res))
-      .catch((error) => setFeedback({ error }))
+      .catch((error) => setFeedback({ error }));
   };
 
   const moveToCart = (product) => {
@@ -29,23 +47,34 @@ const WishlistContent = ({user, wishList, getWishlist, removeFromWishlist, addTo
 
   return (
     <Box sx={{ padding: 2 }}>
-      {wishList.length < 1  || !wishList ? (
+      {wishList?.length < 1 || !wishList ? (
         <Typography>Your wishlist is empty!</Typography>
       ) : (
-        <Grid2 container spacing={3} >
-          {wishList.map(item => (
-            <Grid2 className="wishlist_item" item="true" xs={12} sm={6} md={4} key={item.id}>
+        <Grid2 container spacing={3}>
+          {wishList.map((item) => (
+            <Grid2
+              className="wishlist_item"
+              item="true"
+              xs={12}
+              sm={6}
+              md={4}
+              key={item.id}
+            >
               <Card sx={{ maxWidth: 345 }}>
                 <CardMedia
                   component="img"
                   height="200"
                   image={item.imgs[0]}
                   alt={item.title}
-                  sx={{objectFit: "contain"}}
+                  sx={{ objectFit: "contain" }}
                 />
                 <CardContent>
-                  <Typography variant="h6" component="div">{item.title}</Typography>
-                  <Typography variant="body2" color="text.secondary">Price: GH&#8373;{item.price}</Typography>
+                  <Typography variant="h6" component="div">
+                    {item.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Price: GH&#8373;{item.price}
+                  </Typography>
                   <Button
                     variant="outlined"
                     color="secondary"
@@ -80,12 +109,16 @@ WishlistContent.propTypes = {
   addToCart: PropTypes.func.isRequired,
   removeFromWishlist: PropTypes.func.isRequired,
   setFeedback: PropTypes.func.isRequired,
-}
+};
 
 const mapStateToProps = (state) => ({
   wishList: state.auth.wishList,
-  user: state.auth.user
-})
+  user: state.auth.user,
+});
 
-
-export default connect(mapStateToProps, {getWishlist, removeFromWishlist, addToCart, setFeedback})(WishlistContent)
+export default connect(mapStateToProps, {
+  getWishlist,
+  removeFromWishlist,
+  addToCart,
+  setFeedback,
+})(WishlistContent);
